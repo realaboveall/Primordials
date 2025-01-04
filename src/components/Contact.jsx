b76c1c638d1c604f5a32cbabfb213cb5e5cac420
realaboveall <time.atulya@gmail.com>
2025-01-04 18:16:53 +0530

    second commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -10,36 +10,14 @@ const ImageClipBox = ({ src, clipClass }) => (
 const Contact = () => {
   return (
     <div id="contact" className="my-20 min-h-96 w-screen  px-10">
-      <div className="relative rounded-lg bg-black py-24 text-blue-50 sm:overflow-hidden">
-        <div className="absolute -left-20 top-0 hidden h-full w-72 overflow-hidden sm:block lg:left-20 lg:w-96">
-          <ImageClipBox
-            src="/img/contact-1.webp"
-            clipClass="contact-clip-path-1"
-          />
-          <ImageClipBox
-            src="/img/contact-2.webp"
-            clipClass="contact-clip-path-2 lg:translate-y-40 translate-y-60"
-          />
-        </div>
-
-        <div className="absolute -top-40 left-20 w-60 sm:top-1/2 md:left-auto md:right-10 lg:top-20 lg:w-80 max-sm:right-0 max-sm:scale-[60%]">
-          <ImageClipBox
-            src="/img/swordman-partial.webp"
-            clipClass="absolute md:scale-125"
-          />
-          <ImageClipBox
-            src="/img/swordman.webp"
-            clipClass="sword-man-clip-path md:scale-125"
-          />
-        </div>
-
+      <div className="relative rounded-lg bg-[url('img/footer.jpg')]  py-24 text-blue-50 sm:overflow-hidden">
         <div className="flex flex-col items-center text-center">
           <p className="mb-10 font-general text-[10px] uppercase">
             Join Primordials
           </p>
 
           <AnimatedTitle
-            title="Focus o<b>n Your Game</b><br /> While We <br />Focus<b>o</b>nYou."
+            title="<b>Ignite </b>your <b>passion</b><br />with Our <b>spark</b>."
             className="special-font !md:text-[6.2rem] w-full font-zentry !text-5xl !font-black !leading-[.9]"
           />
 
